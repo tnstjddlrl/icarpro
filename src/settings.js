6ae87daf6b43a02debf1e0ca3f9146d728547467
김동하 <>
2021-04-15 10:22:54 +0900

    예열 시동시간 페이지 이름 변경 및 예열시동시간 버튼 삭제 준비 끝

--- src/settings.js
+++ src/settings.js
@@ -332,7 +332,7 @@ const Settings = () => {
 
             {/* 예열시동 시간 설정 */}
             <View style={{ marginTop: 8 }}></View>
-            <TouchableWithoutFeedback onPress={() => navigation.navigate('예열시동시간')}>
+            <TouchableWithoutFeedback>
               <View style={styles.oneFrame}>
                 <View style={{ marginLeft: 11, marginRight: 16, flexDirection: "row", justifyContent: "space-between" }}>
                   <View style={{ flexDirection: "row" }}>
