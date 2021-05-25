cddfa39013264e43bcc70fb5108ee910a11fd00d
김동하 <>
2021-05-25 16:00:47 +0900

    fcm 메시지 삭제

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ function App() {
 
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
     });
 
     return unsubscribe;
